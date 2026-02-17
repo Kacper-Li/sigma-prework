a76f01ac8203912e72e5c52209df962f79029e25
Kacper <kacperlikus@hotmail.com>
2026-02-17 20:11:31 +0000

    Added testing for the function

--- maxmin.py
+++ maxmin.py
@@ -1,3 +1,10 @@
+def test_value(input, correct_answer):
+    calculated_answer = min_and_max(input)
+    correct = (calculated_answer == correct_answer)
+    print(f"Correct:  {correct}")
+    print(f"Expected: {correct_answer}")
+    print(f"Gotten:   {calculated_answer}")
+
 def min_and_max(array):
     lowest = array[0]
     highest = array[0]
@@ -9,10 +16,12 @@ def min_and_max(array):
     return [lowest, highest]
 
 test1 = [2, 4, 1, 0, 2, -1]
+answer1 = [-1,4]
 test2 = [20, 50, 12, 6, 14, 8]
+answer2 = [6,50]
 test3 = [100, -100]
+answer3 = [-100,100]
 
-def test_value(input, correct_answer):
-    calculated_answer = min_and_max(input)
-    correct = (calculated_answer == correct_answer)
-    print(f"Correct: {correct}, Expected: {correct_answer}, Gotten: {calculated_answer}")
+test_value(test1, answer1)
+test_value(test2, answer2)
+test_value(test3, answer3)
