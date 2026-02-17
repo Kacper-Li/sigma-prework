1abac2c8428d23220fd5a884830516fa5f2dfcd6
Kacper <kacperlikus@hotmail.com>
2026-02-17 21:07:04 +0000

    Created functionality plan

--- age.py
+++ age.py
@@ -1,4 +1,14 @@
-print("Hello World!")
-age = 22
-name = "Kacper"
-print(f"Yo {name} you're {age}? That's kinda old...")
+# Take a date e.g(01-01-1990)
+# Get current date
+# Calculate Difference
+# Output number of years
+
+def age_calculator(date):
+    current_date = "Feb 17 2026"
+    difference = current_date - date
+    whole_years = difference + " some logic that makes sure you count only full years"
+    age = whole_years
+    return age
+
+input_date = input("Enter a date: ")
+age_calculator(input_date)
