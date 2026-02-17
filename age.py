8593899eb3b3304196e3a5e3af936d8d925a0b12
Kacper <kacperlikus@hotmail.com>
2026-02-17 22:27:48 +0000

    Made function return only int of years

--- age.py
+++ age.py
@@ -11,9 +11,10 @@ def age_calculator(date_given: date):
     difference = current_date - date_given
     years = difference.days // 365
     days = difference.days % 365
-    return f"{years} years and {days} days"
+    nicer_output = f"{years} years and {days} days"
+    return years
 
 input_date = input("Enter a date in iso format|YYYY-MM-DD|: ")
 formatted_date = date.fromisoformat(input_date)
 time_difference = age_calculator(formatted_date)
-print(f"{time_difference} since {input_date}")
+print(f"{time_difference} years since {input_date}")
