1c7c40b572d6f999d90200a184aa8ca4e18014a9
Kacper <kacperlikus@hotmail.com>
2026-02-17 22:16:18 +0000

    implemented datetime objects to properly extract and compare dates

--- age.py
+++ age.py
@@ -1,14 +1,18 @@
 # Take a date e.g(01-01-1990)
+# Convert to a datetime object (date.fromisoformat)
 # Get current date
 # Calculate Difference
 # Output number of years
 
-def age_calculator(date):
-    current_date = "Feb 17 2026"
-    difference = current_date - date
-    whole_years = difference + " some logic that makes sure you count only full years"
-    age = whole_years
+from datetime import datetime, timedelta, date
+
+def age_calculator(date_given: date):
+    current_date = date.today()
+    difference = current_date - date_given
+    age = difference.days // 365
     return age
 
-input_date = input("Enter a date: ")
-age_calculator(input_date)
+input_date = input("Enter a date in iso format|YYYY-MM-DD|: ")
+formatted_date = date.fromisoformat(input_date)
+print(formatted_date)
+print(f"{age_calculator(formatted_date)} years since {input_date}")
